cc2cbd70bae165113f163a8185be9cbaa6dc8162
Andrii Holovin <Andygol@users.noreply.github.com>
2023-12-16 00:58:25 +0200

    tf demo

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.52.0"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "trrfm"
+    prefix = "terraform/state"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "GCP region to use"
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  description = "GKE nodes number"
+}
+
+variable "GKE_CLUSTER_NAME" {
+  type        = string
+  default     = "main"
+  description = "GKE cluster name"
+}
